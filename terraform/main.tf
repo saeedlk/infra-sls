a4742964d08e9ce38686bf6700fd5f9635c6cfa9
Saeidlk <slk@romoels-macbook-air.local>
2023-06-13 01:53:42 +0330

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,11 +30,7 @@ resource "aws_instance" "ec2_instance" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
-  vpc_security_group_ids = [var.security_group_id]
-
   key_name = "mumbai-ssh"
-  
-  user_data = "${file("./Terraform/scripts/setup.sh")}"
 
   tags = {
     Name = "ws-tunnel-"
